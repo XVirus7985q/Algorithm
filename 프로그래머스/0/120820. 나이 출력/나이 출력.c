a3db9c83f46f06168b0cf653a1ce2f13e935c2ca
XVirus7985q <mil04273@naver.com>
2024-12-26 11:23:55 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.51 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+     return 2022-(age-1);
+}
+int main(){
+     printf("테스트 1 결과: %d\n", solution(40));    // 출력: 1983
+    printf("테스트 2 결과: %d\n", solution(23)); // 출력: 2000
+
+    int answer = 0;
+    return answer;
+}
