50dc5347c76ca4a8bd682b120f146f1c44afb500
XVirus7985q <mil04273@naver.com>
2024-12-26 11:22:07 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+return num1 / num2;
+}
+
+ int main(){
+     printf("테스트 1 결과: %d\n", solution(10, 5));    // 출력: 2
+    printf("테스트 2 결과: %d\n", solution(7, 2)); // 출력: 3
+    int answer = 0;
+    return answer;
+}
