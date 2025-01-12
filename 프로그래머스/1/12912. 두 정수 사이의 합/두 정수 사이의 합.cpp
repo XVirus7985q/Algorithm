3c8c9bdbe101180d6a96655a0d4cfeee58ecfe95
XVirus7985q <mil04273@naver.com>
2025-01-13 08:59:05 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.cpp
@@ -3,17 +3,32 @@
 
 using namespace std;
 
-long long solution(int a, int b) {
-    long long answer = 0;
-      if ( a == b ){
+// 두 정수 a와 b 사이에 포함된 모든 정수의 합을 계산하는 함수
+long long solution(int a, int b) 
+{
+    long long answer = 0; // 결과를 저장할 변수
+    
+    // a와 b가 같을 경우, 하나의 값만 반환
+    if (a == b) 
+    {
         return a;
-    } else if (a < b) {
-        for (int i = a; i <= b; i++)
-            answer += i;
-    } else {
-        for (int i = b; i <= a; i++)
-            answer += i;
+    } 
+    // a가 b보다 작은 경우, a부터 b까지의 합을 계산
+    else if (a < b) 
+    {
+        for (int i = a; i <= b; i++) 
+        {
+            answer += i; // 현재 값을 answer에 더함
+        }
+    } 
+    // b가 a보다 작은 경우, b부터 a까지의 합을 계산
+    else 
+    {
+        for (int i = b; i <= a; i++) 
+        {
+            answer += i; // 현재 값을 answer에 더함
+        }
     }
-        
-    return answer;
+    
+    return answer; // 계산된 합을 반환
 }
